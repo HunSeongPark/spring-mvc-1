f8dfd1ae404c26df2fc4b9392c5e90a5a3000c37
Hunseong <phunseong@gmail.com>
2022-03-29 04:58:16 +0900

    상품 등록 폼 Controller, thymeleaf html

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -39,6 +39,11 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
     /**
      * 테스트용 데이터
      */
