ee5817054d378811b45a09810a1e631a64022b32
Hunseong <phunseong@gmail.com>
2022-03-29 05:16:22 +0900

    상품 수정 form Controller, thymeleaf html

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -72,6 +72,14 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/editForm";
+    }
+
     /**
      * 테스트용 데이터
      */
