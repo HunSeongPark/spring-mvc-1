5d6633ec135ff2b3e6b1c60dd538d8223bb6007c
Hunseong <phunseong@gmail.com>
2022-03-29 04:43:33 +0900

    상품 상세 Controller, thymeleaf html

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -22,17 +23,20 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
-    // @Autowired
-    // public BasicItemController(ItemRepository itemRepository) {
-    // this.itemRepository = itemRepository;
-    // }
-
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
 
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+
+        return "basic/item";
     }
 
     /**
