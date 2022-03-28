26ffd7a7c96bffc2c1c87e135ec1d1fa1ea30a65
Hunseong <phunseong@gmail.com>
2022-03-29 05:24:39 +0900

    상품 등록 PRG 적용

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -64,12 +65,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(Item item) {
         itemRepository.save(item);
 //        model.addAttribute("item", item);
 
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
